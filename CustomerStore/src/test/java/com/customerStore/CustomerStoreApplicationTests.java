1e75a88c72e1f4bcdb511988df587baa21a4c72b
aniket.bonde <aniket.bonde@ITLNB281.Infraseepz.lan>
2020-06-21 20:15:24 +0530

    Initial commit

---
+++ CustomerStore/src/test/java/com/customerStore/CustomerStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.customerStore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CustomerStoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
